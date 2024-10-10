731db1c188ee6e3b97c4f8f01a623c040fece849
ktaguelmint <kylian.taguelmint@epitech.eu>
2024-10-10 13:00:50 +0200

    Add contact page

--- my-laravel-app/resources/views/contact.blade.php
+++ my-laravel-app/resources/views/contact.blade.php
@@ -1,4 +1,23 @@
 <x-layout>
     @slot('title'){{$title}}@endslot
-    <div>Welcome to my contact page </div>
+    <div class="container">
+        <div class="profile">
+            <h2>My Information</h2>
+            <div class="info-group">
+                <p><strong>Name:</strong> Kylian Taguelmint</p>
+            </div>
+            <div class="info-group">
+                <p><strong>Email:</strong> kyliankylian2@gmail.com</p>
+            </div>
+            <div class="info-group">
+                <p><strong>Instagram:</strong> <a href="https://instagram.com/kylian.tglmnt" target="_blank">kylian.tglmnt</a></p>
+            </div>
+            <div class="info-group">
+                <p><strong>Twitter:</strong> <a href="https://twitter.com/elonmusk" target="_blank">elonmusk</a></p>
+            </div>
+        </div>
+        <div class="footer">
+            <p>&copy; 2024 Your Website. All rights reserved.</p>
+        </div>
+    </div>
 </x-layout>
