8b02f3de5cc641cb84df733412b3885a4aa9845b
ktaguelmint <kylian.taguelmint@epitech.eu>
2024-10-10 12:43:03 +0200

    Add data

--- my-laravel-app/resources/views/post.blade.php
+++ my-laravel-app/resources/views/post.blade.php
@@ -1,15 +1,14 @@
 <x-layout>
     @slot('title'){{$title}}@endslot
-    @foreach($posts as $post)
-        <article class="py-8 max-w-screen-md post-container">
+    <link rel="stylesheet" href="../css/style.css">
+    <article class="py-8 max-w-screen-md post-container">
             <a href="/posts/{{$post->id}}" class="hover:underline">
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post->title }}</h2>
             </a>
             <div class="text-based">
                 <a href="#">{{ $post->author->name }} | {{$post->created_at}}</a>
             </div>
-            <p class="my-4 font-light">{{ Str::limit($post->body,50) }}</p>
-            <a href="/posts/{{$post->id}}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+            <p class="my-4 font-light">{{$post->body}}</p>
+            <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
         </article>
-    @endforeach
 </x-layout>
